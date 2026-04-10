70fbd681dd99756806245e767cf2c884f3e95f8b
bcboliverjames-boop <bcboliverjames@gmail.com>
2026-04-10 14:38:26 +0800

    fix: 添加ICP备案号粤ICP备2026035310号

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,6 +1,6 @@
 export const SITE_NAME = 'SAP 顾问需求广场'
 
-export const ICP_NUMBER = '待填写'
+export const ICP_NUMBER = '粤ICP备2026035310号'
 export const ICP_LINK = 'https://beian.miit.gov.cn/'
 
 export const CONTACT_EMAIL =
